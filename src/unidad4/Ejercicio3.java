6b6c2e3ed313b64bb04b837a6d3322a7b7dba43a
Manuel Ramos <lolin169@gmail.com>
2020-10-31 21:56:06 +0100

    ejercicio 3 de la unidad 4 corregido

--- src/unidad4/Ejercicio3.java
+++ src/unidad4/Ejercicio3.java
@@ -16,29 +16,37 @@ public class Ejercicio3 {
 		System.out.print("Introduzca la segunda cadena: ");
 		String cadena2= teclado.nextLine();
 		
-		int contador=0;
-		int i=0;
-		int j=0;
-
+		boolean mayor2=false;
+        int contador=0;
 		
 		
 			if(cadena1.length()>cadena2.length()) {
 			
-			 i= cadena1.indexOf(cadena2, i);
-			
-			 contador++;
+				while(cadena1.indexOf(cadena2)>-1) {	
+				cadena1=cadena1.substring(cadena1.indexOf(cadena2)+cadena2.length(), cadena1.length());
+		
+				contador++;
 
-			 System.out.println("La cadena mayor es la cadena 1.");}
+				}
+				System.out.print("La cadena mayor es la cadena 1.");
+			}
 		
+			else {
+					while(cadena2.indexOf(cadena1)>-1) {
+					cadena2=cadena2.substring(cadena2.indexOf(cadena1)+cadena1.length(),cadena2.length());
+					
+					contador++;
+					
+					}
+					System.out.print("La cadena mayor es la cadena 2.");
+				}
+			/*if(mayor2=true) {
+					System.out.print("La cadena mayor es la cadena 2.");
+					}
 			
-		else {
-		
-			j=cadena2.indexOf(cadena1, j);
-            contador++;
-			System.out.println("La cadena mayor es la cadena 2.");}
-			
-			
-		
+			else{
+				System.out.print("La cadena mayor es la cadena 1.");
+				}**/
 	  
 		System.out.println("Está contenida "+contador+" veces en la otra.");
 		  
